6399df061d05a560c7b04ee0a7e8a7f7d672bea4
Raphael <raphael.waltenspuel@unibas.ch>
2026-09-15 15:15:02 +0200

    adds optional live test

--- tests/SCIRE.Foundation.Service.Evaluation.Tests/Support/DresLiveFactAttribute.cs
+++ tests/SCIRE.Foundation.Service.Evaluation.Tests/Support/DresLiveFactAttribute.cs
@@ -4,13 +4,23 @@ internal sealed class DresLiveFactAttribute : FactAttribute
 {
     public DresLiveFactAttribute(bool requireViewerState = false)
     {
-        if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_ENDPOINT")) || string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_USERNAME")) || string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_PASSWORD")))
+        if (string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_LIVE_TESTING")) ||
+            string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_ENDPOINT")) ||
+            string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_USERNAME")) ||
+            string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_PASSWORD")))
         {
-            this.Skip = "Set DRES_ENDPOINT, DRES_USERNAME and DRES_PASSWORD to run against a live DRES instance.";
+            this.Skip = "Set DRES_LIVE_TESTING, DRES_ENDPOINT, DRES_USERNAME and DRES_PASSWORD to run against a live DRES instance.";
         }
-        else if (requireViewerState && (Environment.GetEnvironmentVariable("DRES_TEST_VIEWER_STATE") != "1" || string.IsNullOrWhiteSpace(Environment.GetEnvironmentVariable("DRES_EVALUATION_ID"))))
+        else if (requireViewerState && (Environment.GetEnvironmentVariable("DRES_TEST_VIEWER_STATE") != "1" ||
+                                        string.IsNullOrWhiteSpace(
+                                            Environment.GetEnvironmentVariable("DRES_EVALUATION_ID"))))
         {
-            this.Skip = "Set DRES_EVALUATION_ID and DRES_TEST_VIEWER_STATE=1 for a run that permits participant viewing.";
+            this.Skip =
+                "Set DRES_EVALUATION_ID and DRES_TEST_VIEWER_STATE=1 for a run that permits participant viewing.";
+        }
+        else if (Environment.GetEnvironmentVariable("DRES_LIVE_TESTING")?.ToLowerInvariant() == "false")
+        {
+            this.Skip = "Set DRES_LIVE_TESTING=true to run against a live DRES instance.";
         }
     }
 }
